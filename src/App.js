fe738ca6d8836a85c02ca20635499c4ff22330a5
Kat-Str <120758963+Kat-Str@users.noreply.github.com>
2023-08-09 16:18:12 +0200

    Added Categories component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Categories from "./components/categories/categories.component";
+
 function App() {
   return (
     <div className="App">
       <h1>Clothing store</h1>
+      <Categories />
     </div>
   );
 }
